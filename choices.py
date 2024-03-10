43cb553512c227f421d91b5bc3a8ae538e742670
Exon12 <guireg.nael@epitech.eu>
2024-03-10 08:32:59 +0100

    [FIX]: wrong names

--- choices.py
+++ choices.py
@@ -18,14 +18,14 @@ from colorama import Fore, Style
 
 def get_choice():
     while True:
-        print(f"{Fore.LIGHTBLUE_EX}1. Clan{Style.RESET_ALL}")
-        print(f"{Fore.LIGHTBLUE_EX}2. Succession{Style.RESET_ALL}")
-        print(f"{Fore.LIGHTBLUE_EX}3. Academy{Style.RESET_ALL}")
-        print(f"{Fore.LIGHTBLUE_EX}4. Lab{Style.RESET_ALL}")
-        print(f"{Fore.LIGHTBLUE_EX}5. Ally{Style.RESET_ALL}")
-        print(f"{Fore.LIGHTBLUE_EX}6. Ninja Bond{Style.RESET_ALL}")
-        print(f"{Fore.LIGHTBLUE_EX}7. Ranking{Style.RESET_ALL}")
-        print(f"{Fore.LIGHTBLUE_EX}8. Shop{Style.RESET_ALL}")
+        print(f"{Fore.LIGHTBLUE_EX}1. More{Style.RESET_ALL}")
+        print(f"{Fore.LIGHTBLUE_EX}2. Challenge{Style.RESET_ALL}")
+        print(f"{Fore.LIGHTBLUE_EX}3. Ninja{Style.RESET_ALL}")
+        print(f"{Fore.LIGHTBLUE_EX}4. Hire{Style.RESET_ALL}")
+        print(f"{Fore.LIGHTBLUE_EX}5. F. Jutsu{Style.RESET_ALL}")
+        print(f"{Fore.LIGHTBLUE_EX}6. Beasts{Style.RESET_ALL}")
+        print(f"{Fore.LIGHTBLUE_EX}7. Six Path Tools{Style.RESET_ALL}")
+        print(f"{Fore.LIGHTBLUE_EX}8. Other Events{Style.RESET_ALL}")
         print(f"{Fore.LIGHTBLUE_EX}9. Exit{Style.RESET_ALL}")
         
         choice = input(f"{Fore.CYAN}Enter your choice: {Style.RESET_ALL}")
@@ -36,16 +36,16 @@ def get_choice():
 
 def get_more_choices():
     while True:
-        print("1. Clan")
-        print("2. Succession")
-        print("3. Academy")
-        print("4. Lab")
-        print("5. Ally")
-        print("6. Ninja Bond")
-        print("7. Ranking")
-        print("8. Shop")
-        print("9. Archive")
-        print("10. Back")
+        print(f"{Fore.LIGHTBLUE_EX}1. Clan{Style.RESET_ALL}")
+        print(f"{Fore.LIGHTBLUE_EX}2. Succession{Style.RESET_ALL}")
+        print(f"{Fore.LIGHTBLUE_EX}3. Academy{Style.RESET_ALL}")
+        print(f"{Fore.LIGHTBLUE_EX}4. Lab{Style.RESET_ALL}")
+        print(f"{Fore.LIGHTBLUE_EX}5. Ally{Style.RESET_ALL}")
+        print(f"{Fore.LIGHTBLUE_EX}6. Ninja Bond{Style.RESET_ALL}")
+        print(f"{Fore.LIGHTBLUE_EX}7. Ranking{Style.RESET_ALL}")
+        print(f"{Fore.LIGHTBLUE_EX}8. Shop{Style.RESET_ALL}")
+        print(f"{Fore.LIGHTBLUE_EX}9. Archive{Style.RESET_ALL}")
+        print(f"{Fore.LIGHTBLUE_EX}10. Back{Style.RESET_ALL}")
 
         choice = input(f"{Fore.CYAN}Enter your choice: {Style.RESET_ALL}")
         if choice.isdigit() and 1 <= int(choice) <= 10:
